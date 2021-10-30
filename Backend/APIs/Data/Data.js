562c93e2a8e611c7ca778f959725313eaa33b6f9
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-10-30 19:26:33 +0500

    Create Data.js

---
+++ Backend/APIs/Data/Data.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+//Has all MongoDb custom functions
+const db = require('../../Database/Connection')
+// Has all errors
+const errors = require('../Error Messages/ErrorMessages')
+
+const router = express.Router()
+
+router.use(bodyparser.json())
+
+//GET PRODUCT BY CATEGORY API
+router.post("/add", async (req, res) =>{
+    
+})
+
+//SEARCH FOR PRODUCTS API
+router.post("/update", async (req, res) =>{
+    
+})
+
+//REMOVE PRODUCT API
+router.post("/remove", async (req, res) =>{
+    
+})
+
+
+module.exports = router
