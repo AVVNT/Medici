255d8db43eddb605c26bb5af088266ea26767070
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-12-22 04:02:06 +0500

    Update Orders.js

--- Frontend/src/components/Admin Dashboard/Orders.js
+++ Frontend/src/components/Admin Dashboard/Orders.js
@@ -1,3 +1,69 @@
+// import './dashboard.css';
+// import ListOrders from './components/ListOrders';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// export default function Orders() {
+//     const [orders, setOrders] = useState(null)
+//     const [message, setMessage] = useState("")
+//     const [headers, setHeaders] = useState({})
+
+//     useEffect(() => {
+//         console.log("CALLde")
+//         async function fetchMyAPI() {
+//             await getOrders()
+//         }
+
+//         setHeaders({
+//             'Content-Type': 'application/json',
+//             'x-access-token': sessionStorage.getItem('x-token')
+//         })
+
+//         fetchMyAPI()
+//     }, [])
+
+//     async function getOrders(){
+//         let response = await axios.get('http://localhost:3000/api/admin/orders/getall', {headers: headers})
+//         console.log(response.data)
+//         console.log(headers)
+//         setOrders(response.data.data)
+//     }
+
+//     return (
+//         <div className="container">
+//             <div className="containerFormSection">
+//                 <h4>Orders</h4>
+//                 {/* <p>Select Category</p>
+//                 <select
+//                     name='selectedCategory'
+//                     value={selectedCategory}
+//                     onChange={(e)=>{
+//                         setSelectedCategory(e.target.value)
+//                     }}
+//                 >
+//                     {categories.map(item => (
+//                         <option
+//                             key={item._id}
+//                             value={item.name}
+//                         >
+//                             {item.name}
+//                         </option>
+//                     ))}
+//                 </select> */}
+//                 {orders == null ? <p>LOADING</p>
+//                 :
+//                 <ListOrders 
+//                     dataArray={orders}
+//                 />}
+//                 {message === "" ? <></> : <h1>{message}</h1>}
+//             </div>
+//             <div className="containerPreviewSection">
+
+//             </div>
+//         </div >
+//     );
+// }
+
 import './dashboard.css';
 import ListOrders from './components/ListOrders';
 import React, { useEffect, useState } from 'react';
@@ -5,34 +71,64 @@ import axios from 'axios';
 
 export default function Orders() {
     const [orders, setOrders] = useState(null)
+    const [searchFilter, setSearchFilter] = useState('')
+    const [filteredOrders, setFilteredOrders] = useState(null)
     const [message, setMessage] = useState("")
-    const [headers, setHeaders] = useState({})
+    let headers = {}
 
     useEffect(() => {
-        console.log("CALLde")
         async function fetchMyAPI() {
             await getOrders()
         }
 
-        setHeaders({
+        headers = {
             'Content-Type': 'application/json',
             'x-access-token': sessionStorage.getItem('x-token')
-        })
+        }
+
+        console.log(headers)
 
         fetchMyAPI()
     }, [])
 
-    async function getOrders(){
-        let response = await axios.get('http://localhost:3000/api/admin/orders/getall', {headers: headers})
-        console.log(response.data)
-        console.log(headers)
+    useEffect(() => {
+        if (filteredOrders != null)
+            getSearchOrders();
+    }, [searchFilter]);
+
+    async function getOrders() {
+        let response = await axios.get('http://localhost:3000/api/admin/orders/getall', { headers: headers })
         setOrders(response.data.data)
+        setFilteredOrders(response.data.data)
+    }
+
+    function getSearchOrders() {
+        if (searchFilter === "") {
+            setFilteredOrders(orders);
+        } else {
+            let newArray = orders.filter(function (el) {
+                return String(el._id).includes(searchFilter);
+            });
+            setFilteredOrders(newArray);
+        }
     }
 
     return (
         <div className="container">
             <div className="containerFormSection">
                 <h4>Orders</h4>
+                {/* SEARCH */}
+                <div>
+                    <label>Search</label>
+                    <input
+                        type="text"
+                        value={searchFilter}
+                        name="searchFilter"
+                        onChange={(e) => {
+                            setSearchFilter(e.target.value);
+                        }}
+                    />
+                </div>
                 {/* <p>Select Category</p>
                 <select
                     name='selectedCategory'
@@ -50,11 +146,12 @@ export default function Orders() {
                         </option>
                     ))}
                 </select> */}
-                {orders == null ? <p>LOADING</p>
-                :
-                <ListOrders 
-                    dataArray={orders}
-                />}
+                {filteredOrders == null ? <p>LOADING</p>
+                    :
+                    <ListOrders
+                        dataArray={filteredOrders}
+                    />}
+
                 {message === "" ? <></> : <h1>{message}</h1>}
             </div>
             <div className="containerPreviewSection">
