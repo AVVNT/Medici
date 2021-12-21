cb15ab625a121ad9a18dcc5574a229269c77e57e
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-12-22 00:18:49 +0500

    admin login made

---
+++ Frontend/src/components/Admin Dashboard/Login.js
@@ -0,0 +1,124 @@
+import React, {useState} from 'react'
+import axios from 'axios';
+
+import LoginStyle from './LoginStyle.css'
+
+export default function Login({setLoggedInUI}){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [isSending, setIsSending] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [errorVisible, setErrorVisible] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    // Main function that calls the login api
+    async function loginAPI(emailField, passwordField){
+        // To avoid pressing on login button while request is being performed
+        if(isSending)
+            return
+        
+        setIsSending(true)
+        if(emailField == '' || passwordField == ''){
+            setErrorMessage("Empty Fields")
+            setErrorVisible(true)
+            setIsSending(false)
+            return
+        }
+
+        try {
+            // let response = await fetch('http://localhost:3000/api/authentication/login', 
+            // {
+            //     method : 'post',
+            //     headers : {'Content-Type': 'application/json'},
+            //     body : JSON.stringify({
+            //         email : emailField,
+            //         password : passwordField
+            //     })
+            // })
+            let response = await axios.post('http://localhost:3000/api/admin/authentication/login', {
+                'email': emailField, 
+                'password': passwordField
+            }, {headers: {
+                'Content-Type': 'application/json'
+            }})
+            let json = response.data
+            await verifyLogin(json)
+            setIsSending(false)
+        } catch (error) {
+            console.log(error)
+            setIsSending(false)
+        }
+    }
+
+    // Function that checks the response json headers for what error message it has
+    const verifyLogin = async (response) => {
+            if(response.header.error == 0){
+                setErrorMessage("SUCCESS")
+                setErrorVisible(false)
+
+                sessionStorage.setItem('x-token', response.header.token)
+
+                setIsLoggedIn(true)
+
+                setLoggedInUI(true)
+            }
+            // Email not found error
+            else if(response.header.error == 1){
+                setErrorMessage("Email ID does not exist")
+                setErrorVisible(true)
+            }
+            // incorrect password
+            else if(response.header.error == 2){
+                setErrorMessage("Incorrect password")
+                setErrorVisible(true)
+            }
+            // database error
+            else if(response.header.error == 3){
+                setErrorMessage("Database error. Please try again later")
+                setErrorVisible(true)
+            }
+            // other error
+            else{
+                setErrorMessage("Error. Try again later")
+                setErrorVisible(true)
+            }
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        await loginAPI(email, password)
+    }
+
+    return(
+        <div className= "Login">
+            <h1 style= {LoginStyle.h1}>LOGIN</h1>
+            {/* INPUT AREA */}
+           <form onSubmit= {handleSubmit}>
+                <label className= "Label">
+                    <p>EMAIL ID</p>
+                    <input className= "Input" type="text" onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    <p>PASSWORD</p>
+                    <input className= "Input" type="password" onChange={e => setPassword(e.target.value)} />
+                </label>
+                <div>
+                    <button className= "Button" type="submit">LOGIN</button>
+                </div>
+            </form>
+{/*             
+            <div>
+                <button className= "Button" onClick= {() => history.push('/signup')}>SIGNUP</button>
+            </div> */}
+
+            {/* {isLoggedIn ? <Link to= "/dashboard">{console.log("SUCCESS")}</Link>: null} */}
+
+            {/* ERROR MESSAGE AREA */}
+            <div className= "Error-Message">
+                {errorVisible ? <h3>{errorMessage}</h3> : null}
+            </div>
+        </div>
+    )
+
+}
--- frontend/src/Pages/AdminDashboard/AdminDashboard.js
+++ frontend/src/Pages/AdminDashboard/AdminDashboard.js
@@ -4,86 +4,94 @@ import AddProduct from '../../components/Admin Dashboard/AddProduct';
 import RemoveProduct from '../../components/Admin Dashboard/RemoveProduct';
 import { useState } from 'react';
 import UpdateProduct from '../../components/Admin Dashboard/UpdateProduct';
+import Login from '../../components/Admin Dashboard/Login';
 
 function AdminDashboard() {
     const [selectedTab, setSelectedTab] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     return (
-        <div className="dashboard_body primary-font">
-            <div className="dashboard_container">
-                <div className="dashboard_navigation_container">
-                    <div className="dashboard_navigation_container_content">
-                        <div className="dashboard_navigation_container_content_logo">
-                            <img src={MediciLogo} alt="Medici Logo" width="100px" height="100px" />
+        <div>
+            {!isLoggedIn ? <Login
+                setLoggedInUI={setIsLoggedIn}
+            /> :
+                <div className="dashboard_body primary-font">
+                    <div className="dashboard_container">
+                        <div className="dashboard_navigation_container">
+                            <div className="dashboard_navigation_container_content">
+                                <div className="dashboard_navigation_container_content_logo">
+                                    <img src={MediciLogo} alt="Medici Logo" width="100px" height="100px" />
+                                </div>
+                                <div className="dashboard_navigation_container_content_navigation">
+                                    <ul className="navigation_list">
+                                        <li className="navigation_list_item">
+                                            Dashboard
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("add product")
+                                            }}>
+                                                Add Product
+                                            </button>
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("remove product")
+                                            }}>
+                                                Remove Product
+                                            </button>
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("update product")
+                                            }}>
+                                                Update Product
+                                            </button>
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("add category")
+                                            }}>
+                                                Add Category
+                                            </button>
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("remove category")
+                                            }}>
+                                                Remove Category
+                                            </button>
+                                        </li>
+                                        <li className="navigation_list_item">
+                                            <button onClick={() => {
+                                                setSelectedTab("edit category")
+                                            }}>
+                                                Edit Category
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <div className="dashboard_navigation_container_content_navigation">
-                            <ul className="navigation_list">
-                                <li className="navigation_list_item">
-                                    Dashboard
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("add product")
-                                    }}>
-                                        Add Product
-                                    </button>
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("remove product")
-                                    }}>
-                                        Remove Product
-                                    </button>
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("update product")
-                                    }}>
-                                        Update Product
-                                    </button>
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("add category")
-                                    }}>
-                                        Add Category
-                                    </button>
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("remove category")
-                                    }}>
-                                        Remove Category
-                                    </button>
-                                </li>
-                                <li className="navigation_list_item">
-                                    <button onClick={()=>{
-                                        setSelectedTab("edit category")
-                                    }}>
-                                        Edit Category
-                                    </button>
-                                </li>
-                            </ul>
+                        <div className="dashboard_content_container">
+                            <h3>
+                                Dashboard
+                            </h3>
+                            <hr />
+                            {selectedTab === "add product" && (
+                                <AddProduct />
+                            )}
+                            {selectedTab === "remove product" && (
+                                <RemoveProduct />
+                            )}
+                            {selectedTab === "update product" && (
+                                <UpdateProduct />
+                            )}
                         </div>
                     </div>
-                </div>
-                <div className="dashboard_content_container">
-                    <h3>
-                        Dashboard
-                    </h3>
-                    <hr />
-                    {selectedTab === "add product" && (
-                        <AddProduct />
-                    )}
-                    {selectedTab === "remove product" && (
-                        <RemoveProduct />
-                    )}
-                    {selectedTab === "update product" && (
-                        <UpdateProduct />
-                    )}
-                </div>
-            </div>
+                </div>}
         </div>
+
     );
 }
 
